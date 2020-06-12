da8619289fb95cc598ee170c30ab6f916ae818a4
harshasukhdeve <harshasukhdeve0903@gmail.com>
2020-06-12 09:01:56 +0530

    FSJ0420A115

---
+++ testimonials/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { DescriptionComponent } from './description/description.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BodyComponent,
+    FooterComponent,
+    HeaderComponent,
+    DescriptionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
